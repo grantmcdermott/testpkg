55005b99355e9b640577e7a73a538b0633df3948
Grant McDermott <grant.mcdermott@gmail.com>
2024-02-27 21:31:28 -0800

    move tests to submodule

--- inst/tinytest/helpers.R
+++
@@ -1,5 +0,0 @@
-library(tinytest)
-library(tinysnapshot)
-options("tinysnapshot_os" = "Linux")
-options("tinysnapshot_device" = "svglite")
-options("tinysnapshot_device_args" = list(user_fonts = fontquiver::font_families("Liberation")))
--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,6 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  tinytest::test_package("testpkg")
+  # tinytest::test_package("testpkg")
+  tinytest::test_package("testpkg", testdir="testsuite/tinytest")
 }
 
