96e8eb296e198a1ab8ee299c56368110d933d8ba
Grant McDermott <gmcd@amazon.com>
2024-02-28 17:36:17 -0800

    skip on cran

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,6 @@
-if (requireNamespace("tinytest", quietly = TRUE)) {
+# skip tests on CRAN
+run_tests = identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+
+if (run_tests && requireNamespace("tinytest", quietly = TRUE)) {
   tinytest::test_package("testpkg", testdir = "testsuite")
 }
-
